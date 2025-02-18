2dc842394a0ea467e1dd706ae9239ce01decdd88
dennihous <denhouston1@icloud.com>
2025-02-18 12:30:16 +0000

    Models added for Category, Order, OrderItem, Product, User

---
+++ Models/Order.cs
@@ -0,0 +1,8 @@
+public class Order
+{
+    public int Id { get; set; }
+    public int UserId { get; set; }
+    public User User { get; set; }
+    public DateTime OrderDate { get; set; }
+    public List<OrderItem> OrderItems { get; set; }
+}
